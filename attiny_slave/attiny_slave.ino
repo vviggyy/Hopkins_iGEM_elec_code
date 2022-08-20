bd1d205412e8d7a25c30773694aded140a0b4c2b
Vignesh Vanchinathan <viggy.vanchi@gmail.com>
2022-08-19 20:28:01 -0400

    changed clock speed and added notes

--- attiny_slave/attiny_slave.ino
+++ attiny_slave/attiny_slave.ino
@@ -1,10 +1,17 @@
 #include <Wire.h>
 #include <PWM.h>
 
+#define CLK 16000000UL // 20 mHz or 16 mHz, figure out CLK 
+#define shift 90
+
+#define address 0x36 //define slave address, Q: need to <<1 ?  
+
 //pins for PWM, from circuit diagram
 int pwm1 = 3;
 int pwm2 = 5;
 
+int pwm1_toggle = null;
+int pwm2_toggle = null;
 
 //incoming msg from master contains 4 bits
 //long msg = null;
@@ -17,8 +24,8 @@ int frequency = null;
 
 
 void setup() {
+  
   Wire.begin(address);
-  Wire.onReceive(parseMsg);
   
   pinMode(3, OUTPUT);
   pinMode(5, OUTPUT);
@@ -29,7 +36,8 @@ void setup() {
 void parseMsg(char msg[]){
   
   enable = msg[0]
-  freqOpt = strtol(msg[1:2], NULL, 2);
+  freqOpt = strtol(msg[1:2], NULL, 2); //receive a standard i2c message as length of 8 bytes, parse that not a string 
+  //check if first four bits and last four bits are equal and if message is garbled then ignore
   wave = msg[3]
 
   switch(freqOpt){
@@ -51,12 +59,22 @@ void parseMsg(char msg[]){
  SetPinFrequencySafe(pwm2, frequency) //set freq pwm2
 }
 void loop() {
+  
+  Wire.onReceive(parseMsg); //type check; receive as string or bits?
+
   delay(100);
   if (enable == '1') {
 
-    pwmWrite(pwm1, 127) //50% duty cycle pwm1
-    pwmWrite(pwm2, 127) //50% duty cycle pwm2
+    //pwmWrite(pwm1, 127) //50% duty cycle pwm1
+    //pwmWrite(pwm2, 127) //50% duty cycle pwm2
     //TODO phase shift
+
+    pwm1_toggle = (CLK / frequency) / 2;
+    pwm2_toggle = (pwm1_toggle * SHIFT) / 180UL;
+
+    pwm1 = pwm1_toggle;
+    pwm2 = pwm2_toggle;
+    
     
   } else {
     continue; //move back
