5aff1a237d2bec7bb7ce643ae9061e06a9e75de3
sreenieadara <sreeni.eadara@gmail.com>
2022-09-23 21:03:24 -0400

    This code works for recieving integer

--- msg_decode/msg_decode.ino
+++ msg_decode/msg_decode.ino
@@ -1,15 +1,15 @@
 #include <Wire.h>
 
 #define SLAVE_ADDRESS 0x36
-#define MEM_ADDRESS 0x3F00 //SRAM
+//#define MEM_ADDRESS 0x3F00 //SRAM
 
-int LED_PIN = 4;
+int LED_PIN = 13;
 int numTransactions = 1;
 boolean led_status = false;
 
-int dig1 = 0
-int dig2 = 0
-int dig3 = 0
+int dig1 = 0;
+int dig2 = 0;
+int dig3 = 0;
 
 void setup() {
   
@@ -32,12 +32,29 @@ void parseMsg(int eventNum) {
   
   int x = Wire.read();
   
-  char buf[100];
-  sprintf(buf, %03i, x); //separates integers into chars
+  Serial.println("RECIEVED INT:");
+  Serial.println(x);
+  Serial.println();
 
-  dig1 = buf[0]
-  dig2 = buf[1]
-  dig3 = buf[2]
+  char buf[100];
+  sprintf(buf, "%03i", x); //separates integers into chars
+
+  dig1 = buf[0];
+  
+  dig2 = buf[1];
+  dig3 = buf[2];
+
+  Serial.println("Digit 1:");
+  Serial.println(dig1);
+  Serial.println();
+
+  Serial.println("Digit 2:");
+  Serial.println(dig2);
+  Serial.println();
+
+  Serial.println("Digit 3:");
+  Serial.println(dig3);
+  Serial.println();
 
   if (dig1 != 0 && dig2 != 0 && dig3 != 0) {
     digitalWrite(LED_PIN, !led_status);
